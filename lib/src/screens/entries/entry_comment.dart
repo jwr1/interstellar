47bd495fcb53d06c6c48f7e9569460b983b66321
John Wesley <dev@jwr.one>
2023-12-15 11:42:57 -0500

    Add votes and boosts to comments, adjust comment spacing

--- lib/src/screens/entries/entry_comment.dart
+++ lib/src/screens/entries/entry_comment.dart
@@ -14,7 +14,7 @@ class EntryComment extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
-        padding: const EdgeInsets.fromLTRB(8, 8, 2, 8),
+        padding: const EdgeInsets.fromLTRB(8, 8, 0, 0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
@@ -38,11 +38,35 @@ class EntryComment extends StatelessWidget {
                     timeDiffFormat(comment.createdAt),
                     style: const TextStyle(fontWeight: FontWeight.w300),
                   ),
+                ),
+                const Spacer(),
+                IconButton(
+                  icon: const Icon(Icons.more_vert),
+                  onPressed: () {},
+                ),
+                const SizedBox(width: 12),
+                IconButton(
+                  icon: const Icon(Icons.rocket_launch),
+                  onPressed: () {},
+                ),
+                Text(intFormat(comment.uv)),
+                const SizedBox(width: 12),
+                IconButton(
+                  icon: const Icon(Icons.arrow_upward),
+                  onPressed: () {},
+                ),
+                Text(intFormat(comment.favourites - comment.dv)),
+                IconButton(
+                  icon: const Icon(Icons.arrow_downward),
+                  onPressed: () {},
+                ),
+                const SizedBox(
+                  width: 4,
                 )
               ],
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 12),
+              padding: const EdgeInsets.symmetric(vertical: 8),
               child: Markdown(comment.body),
             ),
             if (comment.childCount > 0)
