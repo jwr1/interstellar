266d2d992969708e1fa9fefa4b98b26df3d926b2
Christian <christianstrodl1@gmail.com>
2024-02-12 15:47:25 +1000

    Adds cover image to profile page. Adds optional border for avatars. Updates profile page to show username with and without instance info. Clicking on username with instance info copies to clipboard.

--- lib/src/screens/explore/user_screen.dart
+++ lib/src/screens/explore/user_screen.dart
@@ -11,6 +11,7 @@ import 'package:interstellar/src/widgets/avatar.dart';
 import 'package:interstellar/src/widgets/markdown.dart';
 import 'package:interstellar/src/widgets/text_editor.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
 
 class UserScreen extends StatefulWidget {
   final int userId;
@@ -52,118 +53,156 @@ class _UserScreenState extends State<UserScreen> {
       source: FeedSourceUser(widget.userId),
       title: _data?.username ?? '',
       details: _data != null
-          ? Padding(
-              padding: const EdgeInsets.all(12),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+        ? Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                alignment: Alignment.bottomLeft,
                 children: [
-                  Row(
-                    children: [
-                      if (_data!.avatar?.storageUrl != null)
-                        Padding(
-                          padding: const EdgeInsets.only(right: 12),
-                          child: Avatar(_data!.avatar!.storageUrl, radius: 32),
-                        ),
-                      Expanded(
-                        child: Text(
-                          _data!.username,
-                          style: Theme.of(context).textTheme.titleLarge,
-                          softWrap: true,
-                        ),
-                      ),
-                      OutlinedButton(
-                        style: ButtonStyle(
-                            foregroundColor: _data!.isFollowedByUser == true
-                                ? null
-                                : MaterialStatePropertyAll(
-                                    Theme.of(context).disabledColor)),
-                        onPressed: whenLoggedIn(context, () async {
-                          var newValue = await api_users.putFollow(
-                              context.read<SettingsController>().httpClient,
-                              context.read<SettingsController>().instanceHost,
-                              _data!.userId,
-                              !_data!.isFollowedByUser!);
-
-                          setState(() {
-                            _data = newValue;
-                          });
-                          if (widget.onUpdate != null) {
-                            widget.onUpdate!(newValue);
-                          }
-                        }),
-                        child: Row(
-                          children: [
-                            const Icon(Icons.group),
-                            Text(' ${intFormat(_data!.followersCount)}'),
-                          ],
-                        ),
-                      ),
-                      if (!_data!.username.contains('@'))
-                        IconButton(
-                          onPressed: () {
-                            setState(() {
-                              _messageController = TextEditingController();
-                            });
-                          },
-                          icon: const Icon(Icons.mail),
-                          tooltip: 'Send message',
-                        )
-                    ],
+                  if (_data!.cover != null)
+                    Image.network(
+                      _data!.cover!.storageUrl,
+                      width: _data!.cover!.width.toDouble(),
+                    ),
+                  Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Avatar(_data!.avatar?.storageUrl, radius: 32, borderRadius: 5,),
                   ),
-                  if (_messageController != null)
-                    Column(children: [
-                      TextEditor(
-                        _messageController!,
-                        isMarkdown: true,
-                        label: 'Message',
-                      ),
-                      const SizedBox(height: 8),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          OutlinedButton(
-                            onPressed: () async {
-                              setState(() {
-                                _messageController = null;
-                              });
-                            },
-                            child: const Text('Cancel'),
-                          ),
-                          FilledButton(
-                            onPressed: () async {
-                              final newThread = await postMessage(
-                                context.read<SettingsController>().httpClient,
-                                context.read<SettingsController>().instanceHost,
-                                _data!.userId,
-                                _messageController!.text,
-                              );
-
-                              setState(() {
-                                _messageController = null;
-
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                    builder: (context) => MessageThreadScreen(
-                                      initData: newThread,
-                                    ),
-                                  ),
-                                );
-                              });
-                            },
-                            child: const Text('Send'),
-                          )
-                        ],
-                      )
-                    ]),
-                  if (_data!.about != null)
-                    Padding(
-                      padding: const EdgeInsets.only(top: 12),
-                      child: Markdown(_data!.about!),
-                    )
                 ],
               ),
-            )
-          : null,
+              Padding(
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              _data!.username.contains('@') ? _data!.username.split('@')[1] : _data!.username,
+                              style: Theme.of(context).textTheme.titleLarge,
+                              softWrap: true,
+                            ),
+                            InkWell(
+                              onTap: () async {
+                                await Clipboard.setData(ClipboardData(text: _data!.username.contains('@')
+                                    ? _data!.username
+                                    : '@${_data!.username}@${context.read<SettingsController>().instanceHost}'));
+                              },
+                              child: Text(
+                                _data!.username.contains('@')
+                                    ? _data!.username
+                                    : '@${_data!.username}@${context.read<SettingsController>().instanceHost}',
+                                softWrap: true,
+                              ),
+                            )
+                          ],
+                        ),
+                        Expanded(
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: OutlinedButton(
+                              style: ButtonStyle(
+                                foregroundColor: _data!.isFollowedByUser == true
+                                    ? null
+                                    : MaterialStatePropertyAll(
+                                      Theme.of(context).disabledColor
+                                    )
+                              ),
+                              onPressed: whenLoggedIn(context, () async {
+                                var newValue = await api_users.putFollow(
+                                  context.read<SettingsController>().httpClient,
+                                  context.read<SettingsController>().instanceHost,
+                                  _data!.userId,
+                                  !_data!.isFollowedByUser!
+                                );
+                                setState(() {
+                                  _data = newValue;
+                                });
+                                if (widget.onUpdate != null) {
+                                  widget.onUpdate!(newValue);
+                                }
+                              }),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  const Icon(Icons.group),
+                                  Text(' ${intFormat(_data!.followersCount)}'),
+                                ],
+                              ),
+                            )
+                          )
+                        ),
+                        if (!_data!.username.contains('@'))
+                          IconButton(
+                            onPressed: () {
+                              setState(() {
+                                _messageController = TextEditingController();
+                              });
+                            },
+                            icon: const Icon(Icons.mail),
+                            tooltip: 'Send message',
+                          )
+                      ],
+                    ),
+                    if (_messageController != null)
+                      Column(children: [
+                        TextEditor(
+                          _messageController!,
+                          isMarkdown: true,
+                          label: 'Message',
+                        ),
+                        const SizedBox(height: 8),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            OutlinedButton(
+                              onPressed: () async {
+                                setState(() {
+                                  _messageController = null;
+                                });
+                              },
+                              child: const Text('Cancel'),
+                            ),
+                            FilledButton(
+                              onPressed: () async {
+                                final newThread = await postMessage(
+                                  context.read<SettingsController>().httpClient,
+                                  context.read<SettingsController>().instanceHost,
+                                  _data!.userId,
+                                  _messageController!.text,
+                                );
+
+                                setState(() {
+                                  _messageController = null;
+
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                      builder: (context) => MessageThreadScreen(
+                                        initData: newThread,
+                                      ),
+                                    ),
+                                  );
+                                });
+                              },
+                              child: const Text('Send'),
+                            )
+                          ],
+                        )
+                      ]),
+                    if (_data!.about != null)
+                      Padding(
+                        padding: const EdgeInsets.only(top: 12),
+                        child: Markdown(_data!.about!),
+                      )
+                  ],
+                ),
+              )
+            ]
+          )
+        : null
     );
   }
 }
--- lib/src/widgets/avatar.dart
+++ lib/src/widgets/avatar.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 
 class Avatar extends StatelessWidget {
-  final String url;
+  final String? url;
   final double? radius;
+  final double? borderRadius;
 
-  const Avatar(this.url, {super.key, this.radius});
+  const Avatar(this.url, {super.key, this.radius, this.borderRadius});
 
   @override
   Widget build(BuildContext context) {
     return CircleAvatar(
-      backgroundColor: Colors.transparent,
-      backgroundImage: NetworkImage(url),
-      radius: radius,
+      radius: radius != null && borderRadius != null ? radius! + borderRadius! : radius,
+      child: CircleAvatar(
+        backgroundColor: url != null ? Colors.transparent : null,
+        backgroundImage: url != null ? NetworkImage(url!) : null,
+        radius: radius,
+      )
     );
   }
 }
