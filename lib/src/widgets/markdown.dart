3d592b706992e8435bc05ca48b7b5b4c671927c1
Christian <christianstrodl1@gmail.com>
2024-02-14 16:29:47 +1000

    Adds highlighting for user mentions.

--- lib/src/widgets/markdown.dart
+++ lib/src/widgets/markdown.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:markdown/markdown.dart' as md;
 import 'package:flutter_markdown/flutter_markdown.dart' as flutter_markdown;
 import 'package:interstellar/src/widgets/open_webpage.dart';
+import 'package:provider/provider.dart';
+import 'package:interstellar/src/screens/explore/user_screen.dart';
+import 'package:interstellar/src/api/users.dart' as api_users;
+import 'package:interstellar/src/screens/settings/settings_controller.dart';
 
 class Markdown extends StatelessWidget {
   final String data;
@@ -20,6 +25,62 @@ class Markdown extends StatelessWidget {
         if (href != null) {
           openWebpage(context, Uri.parse(href));
         }
+      },
+      builders: <String, flutter_markdown.MarkdownElementBuilder>{
+        'mention': MentionBuilder()
+      },
+      inlineSyntaxes: [
+        MentionSyntax()
+      ],
+    );
+  }
+}
+
+class MentionSyntax extends md.InlineSyntax {
+
+  static const String _pattern = "@([a-zA-Z0-9._-]+)(@([a-zA-Z0-9._-]+))*";
+
+  MentionSyntax() : super(_pattern);
+  
+  @override
+  RegExp get pattern => RegExp(_pattern);
+
+  @override
+  bool onMatch(md.InlineParser parser, Match match) {
+    String username = match[0]!;
+    parser.addNode(md.Element.text("mention", username));
+    return true;
+  }
+}
+
+class MentionBuilder extends flutter_markdown.MarkdownElementBuilder {
+  @override
+  Widget visitElementAfter(md.Element element, TextStyle? preferredStyle) {
+    return Builder(
+      builder: (context) {
+        return InkWell(
+          onTap: () async {
+            final user = await api_users.fetchUserByName(
+              context.read<SettingsController>().httpClient,
+              context.read<SettingsController>().instanceHost,
+              element.textContent);
+
+            if (!context.mounted) return;
+
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => UserScreen(
+                user.userId,
+                initData: user,
+              ))
+            );
+          },
+          child: Text(
+            element.textContent,
+            style: const TextStyle(
+              color: Colors.blue
+            ),
+          ),
+        );
       },
     );
   }
