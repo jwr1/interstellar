e4a7992d59c5b7bfc2d50116e640a0adfbb1df3b
jwr1 <47087725+jwr1@users.noreply.github.com>
2024-10-02 12:19:45 -0400

    Redesign message correspondence screen

--- lib/src/screens/profile/messages/message_item.dart
+++ lib/src/screens/profile/messages/message_item.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:interstellar/src/models/message.dart';
-import 'package:interstellar/src/screens/explore/user_screen.dart';
 import 'package:interstellar/src/screens/settings/settings_controller.dart';
-import 'package:interstellar/src/widgets/display_name.dart';
-import 'package:interstellar/src/widgets/markdown/markdown.dart';
+import 'package:interstellar/src/utils/utils.dart';
+import 'package:interstellar/src/widgets/avatar.dart';
 import 'package:provider/provider.dart';
 
 class MessageItem extends StatelessWidget {
@@ -25,41 +24,16 @@ class MessageItem extends StatelessWidget {
                 .first)
         .first;
 
-    return Card(
-      clipBehavior: Clip.antiAlias,
-      margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-      child: InkWell(
-        onTap: onClick,
-        child: Padding(
-          padding: const EdgeInsets.all(12),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(right: 10),
-                    child: DisplayName(
-                      messageUser.name,
-                      icon: messageUser.avatar,
-                      onTap: () => Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => UserScreen(messageUser.id),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 8),
-                child: Markdown(item.messages.first.body,
-                    context.watch<SettingsController>().instanceHost),
-              ),
-            ],
-          ),
-        ),
+    return ListTile(
+      title: Text(messageUser.name),
+      subtitle: Text(
+        item.messages.first.body,
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
       ),
+      leading: Avatar(messageUser.avatar),
+      trailing: Text('${dateDiffFormat(item.messages.first.createdAt)} ago'),
+      onTap: onClick,
     );
   }
 }
