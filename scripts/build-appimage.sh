84f1cf3edff4695aa3480a275b43b692c683a513
jwr1 <47087725+jwr1@users.noreply.github.com>
2025-02-19 15:06:25 -0500

    Update AppImage build script based on @Samueru-sama's recommendations

--- scripts/build-appimage.sh
+++ scripts/build-appimage.sh
@@ -2,49 +2,39 @@
 
 set -eu
 
-EXE_BUNDLE_DIR=$(echo build/linux/*/release/bundle)
+export APPIMAGE_EXTRACT_AND_RUN=1
+export ARCH="$(uname -m)"
+
 BUILD_DIR=$(mktemp -d)
 
-export ARCH="$(uname -m)"
-export APPIMAGE_EXTRACT_AND_RUN=1
-
-LIB4BN="https://github.com/VHSgunzo/sharun/releases/latest/download/sharun-$ARCH-aio"
-URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
+LIB4BN_URL="https://github.com/VHSgunzo/sharun/releases/latest/download/sharun-$ARCH-aio"
+APPIMAGETOOL_URL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$ARCH.AppImage"
 
 # Prepare AppDir
-mkdir -p "$BUILD_DIR"/AppDir/usr/share/applications "$BUILD_DIR"/AppDir/shared/lib
-
-cp -r "$EXE_BUNDLE_DIR"/data "$BUILD_DIR"/AppDir
+cp -r build/linux/*/release/bundle/. "$BUILD_DIR"/AppDir
 cp linux/appimage/interstellar.desktop "$BUILD_DIR"/AppDir
-cp linux/appimage/interstellar.desktop "$BUILD_DIR"/AppDir/usr/share/applications
 cp assets/icons/logo.png "$BUILD_DIR"/AppDir/interstellar.png
-cp assets/icons/logo.png "$BUILD_DIR"/AppDir/.DirIcon
 
-ln -s ./usr/share "$BUILD_DIR"/AppDir
-
-# ADD LIBRARIES
-wget "$LIB4BN" -O "$BUILD_DIR"/lib4bin
+# Add libraries
+wget "$LIB4BN_URL" -O "$BUILD_DIR"/lib4bin
 chmod +x "$BUILD_DIR"/lib4bin
-xvfb-run -a -- "$BUILD_DIR"/lib4bin l -p -v -e -r -k -d "$BUILD_DIR"/AppDir "$EXE_BUNDLE_DIR"/interstellar
-
-find "$BUILD_DIR"/AppDir/shared -type f -exec strip -s -R .comment --strip-unneeded {} ';'
+xvfb-run -a -- "$BUILD_DIR"/lib4bin l -s -k -e -v -p -d "$BUILD_DIR"/AppDir "$BUILD_DIR"/AppDir/interstellar /usr/lib/*/libGL*
+ln -s ../data "$BUILD_DIR"/AppDir/bin/data
+ln -s ../lib "$BUILD_DIR"/AppDir/bin/lib
+ln -s ../../data "$BUILD_DIR"/AppDir/shared/bin/data
+ln -s ../../lib "$BUILD_DIR"/AppDir/shared/bin/lib
 
 # Prepare sharun
-ln -s ./bin/interstellar "$BUILD_DIR"/AppDir/AppRun
+ln "$BUILD_DIR"/AppDir/sharun "$BUILD_DIR"/AppDir/AppRun
 "$BUILD_DIR"/AppDir/sharun -g
 
-# MAKE APPIMAGE WITH URUNTIME
-wget -q "$URUNTIME" -O "$BUILD_DIR"/uruntime
-chmod +x "$BUILD_DIR"/uruntime
+# Make AppImage
+wget "$APPIMAGETOOL_URL" -O "$BUILD_DIR"/appimagetool
+chmod +x "$BUILD_DIR"/appimagetool
+"$BUILD_DIR"/appimagetool --comp zstd \
+	--mksquashfs-opt -Xcompression-level --mksquashfs-opt 22 \
+	-n "$BUILD_DIR"/AppDir dist/interstellar-linux-$ARCH.AppImage
 
-echo "Generating AppImage..."
-mkdir -p dist
-"$BUILD_DIR"/uruntime --appimage-mkdwarfs -f \
-	--set-owner 0 --set-group 0 \
-	--no-history --no-create-timestamp \
-	--compression zstd:level=22 -S24 -B16 \
-	--header "$BUILD_DIR"/uruntime \
-	-i "$BUILD_DIR"/AppDir -o dist/interstellar-linux-"$ARCH".AppImage
-
+# Cleanup
 rm -r "$BUILD_DIR"
-echo "All Done!"
+echo "All done!"
