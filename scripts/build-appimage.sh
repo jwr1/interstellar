02e6addad14ec070157e59249e18ed291f61bf4b
jwr1 <47087725+jwr1@users.noreply.github.com>
2025-02-19 22:57:49 -0500

    Add a script to build the AppImage with sharun and uruntime (#101)
    * Add a script to build the AppImage with sharun and uruntime
    
    * Update AppImage build script based on @Samueru-sama's recommendations
    
    * Switch to using uruntime again for AppImage due to smaller output
    
    * Fix browser links not opening with AppImage
    
    * Set Flutter version in GH workflow to hopefully fix Linux ARM build

---
+++ scripts/build-appimage.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+set -eu
+
+export APPIMAGE_EXTRACT_AND_RUN=1
+export ARCH="$(uname -m)"
+
+BUILD_DIR=$(mktemp -d)
+
+LIB4BN_URL="https://github.com/VHSgunzo/sharun/releases/latest/download/sharun-$ARCH-aio"
+URUNTIME_URL="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
+
+# Prepare AppDir
+cp -r build/linux/*/release/bundle/. "$BUILD_DIR"/AppDir
+cp linux/appimage/interstellar.desktop "$BUILD_DIR"/AppDir
+cp assets/icons/logo.png "$BUILD_DIR"/AppDir/interstellar.png
+
+# Add libraries
+wget "$LIB4BN_URL" -O "$BUILD_DIR"/lib4bin
+chmod +x "$BUILD_DIR"/lib4bin
+xvfb-run -a -- "$BUILD_DIR"/lib4bin l -s -k -e -v -p -d "$BUILD_DIR"/AppDir "$BUILD_DIR"/AppDir/interstellar /usr/lib/*/libGL*
+ln -s ../data "$BUILD_DIR"/AppDir/bin/data
+ln -s ../lib "$BUILD_DIR"/AppDir/bin/lib
+ln -s ../../data "$BUILD_DIR"/AppDir/shared/bin/data
+ln -s ../../lib "$BUILD_DIR"/AppDir/shared/bin/lib
+
+# Fix browser links not opening (app expects gio-launch-desktop but can't find it).
+echo '#!/bin/sh
+shift
+xdg-open "$@"
+' > "$BUILD_DIR"/AppDir/bin/gio-launch-desktop
+chmod +x "$BUILD_DIR"/AppDir/bin/gio-launch-desktop
+
+# Prepare sharun
+ln "$BUILD_DIR"/AppDir/sharun "$BUILD_DIR"/AppDir/AppRun
+"$BUILD_DIR"/AppDir/sharun -g
+
+# Make AppImage
+wget "$URUNTIME_URL" -O "$BUILD_DIR"/uruntime
+chmod +x "$BUILD_DIR"/uruntime
+mkdir -p dist
+"$BUILD_DIR"/uruntime --appimage-mkdwarfs -f \
+	--set-owner 0 --set-group 0 \
+	--no-history --no-create-timestamp \
+	--compression zstd:level=22 -S25 -B16 \
+	--header "$BUILD_DIR"/uruntime \
+	-i "$BUILD_DIR"/AppDir -o dist/interstellar-linux-$ARCH.AppImage
+
+# Cleanup
+rm -r "$BUILD_DIR"
+echo "All done!"
